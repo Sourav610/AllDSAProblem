ae12adcf994523af806cc03a1ecc7f091a47cc51
sonu kumar <sk03321@outlook.com>
2024-12-16 23:34:01 +0530

    hasing

--- Hashing/lowerCharacterHashing.java
+++ Hashing/lowerCharacterHashing.java
@@ -17,6 +17,11 @@ public class lowerCharacterHashing {
             arr[i] = br.readLine().charAt(0);
         }
 
+        //for lower character only/ or upper character only
+        // we can store like - character - 'a' , it will give the value like for 'f'
+        //For example, if the given character is ‘f’, we will get the value as (‘f’ - ‘a’) = (102-97) = 5.
+        
+        //for all type of character;
         int hash[] = new int[256];
         for(int i = 0; i<n; i++){
             hash[arr[i]] += 1;
