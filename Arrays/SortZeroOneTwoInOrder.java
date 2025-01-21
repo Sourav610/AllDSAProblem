e8ca53d3fa83edd3722fdfd5e75f8c98ddea5c90
sonu kumar <sk03321@outlook.com>
2025-01-21 21:56:58 +0530

    question solve of array

---
+++ Arrays/SortZeroOneTwoInOrder.java
@@ -0,0 +1,50 @@
+package Arrays;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SortZeroOneTwoInOrder {
+    public static void main(String []args) throws IOException{
+        int n; 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the size of the array: ");
+        n = Integer.parseInt(br.readLine());
+
+        int arr[] = new int[n];
+        System.out.print("Enter "+n+" element: ");
+        for(int i = 0; i<n; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        //Using three pointer approach
+        //l = 0, m = 0, h = n-1, always check m if it is one increment it, if it is two swap with hihg, if it is zero swap with low
+        sort(arr);
+
+        System.out.println("Element after sorting: ");
+        for(int i = 0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+    public static void sort(int []arr){
+        int l = 0, m = 0, h = arr.length-1;
+        while(m<=h){
+            if(arr[m] == 2){
+                int temp = arr[h];
+                arr[h] = arr[m];
+                arr[m] = temp;
+                h--;
+            }
+            else if( arr[m] == 0){
+                int temp = arr[l];
+                arr[l] = arr[m];
+                arr[m] = temp;
+                l++;
+                m++;
+            }
+            else{
+                m++;
+            }
+        }
+    }
+}
