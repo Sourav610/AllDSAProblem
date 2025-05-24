86602829795d2728c8f74695a3d1fa2fab7ca162
sonu kumar <sk03321@outlook.com>
2025-05-24 23:23:28 +0530

    added recurion of atoi

--- String/StringToInteger.java
+++ String/StringToInteger.java
@@ -11,7 +11,8 @@ public class StringToInteger {
         String st = br.readLine();
 
         int ans;
-        ans = convertToInt(st);
+        // ans = convertToInt(st);
+        ans = convertToIntUsingRecursion(0,st,0,1,0);
 
         System.out.println("The converted string to int is:"+ans);
     }
@@ -40,5 +41,37 @@ public class StringToInteger {
         }
 
         return (int)ans*sign;
+    }
+
+    public static int convertToIntUsingRecursion(int i,String str,int ans,int sign,int ch){
+        if(i >=str.length()){
+            return (int)sign*ans;
+        }
+
+        if(sign*ans > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+
+        if(sign*ans <Integer.MIN_VALUE) return Integer.MIN_VALUE;
+        if(str.charAt(i) == ' ' && ch == 0){
+            i = i+1;
+            return convertToIntUsingRecursion(i++,str,ans,sign,0);
+        }
+
+        if(str.charAt(i) == '-' && ch == 0){
+            sign = -1;
+            i = i+1;
+            return convertToIntUsingRecursion(i++,str,ans,sign,1);
+        }
+
+        if(str.charAt(i) == '+' && ch == 0){
+            i = i+1;
+            return convertToIntUsingRecursion(i++,str,ans,sign,1);
+        }
+
+        if(Character.isDigit(str.charAt(i))){
+            ans = ans*10 + str.charAt(i)-'0';
+            i = i+1;
+            return convertToIntUsingRecursion(i,str,ans,sign,1);
+        }
+        return sign*ans;
     }
 }
