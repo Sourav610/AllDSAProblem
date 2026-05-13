353407aaf5c6fbb7500357253105b038011b566b
sonu kumar <sk03321@outlook.com>
2026-05-13 23:41:09 +0530

    added approach

--- BinaryTree/LCABinaryTree.java
+++ BinaryTree/LCABinaryTree.java
@@ -22,6 +22,20 @@ public class LCABinaryTree{
     }
 
     /*
+
+        Start by checking if the current root node is null or matches one of the target nodes (x or y). 
+        If the root is null or matches either target node, return the root, as it could be the LCA or 
+        simply indicate the end of the search path.
+    Perform a recursive search in the left subtree to find nodes x and y by calling the LCA function on the left
+     child of the current root.
+    Similarly, perform a recursive search in the right subtree by calling the LCA function on the right 
+    child of the current root.
+    After completing the recursive searches, analyze the results of both subtree searches:
+    If both recursive calls return non-null values, it means one target node was found in each subtree.
+     In this case, the current root node must be the LCA, as it is the common ancestor of both nodes.
+    If only one of the subtree searches returns a non-null result, 
+    it implies both target nodes are located within the same subtree. 
+    Return the non-null result, which represents the LCA found in that subtree.
     T.C - O(n), S.c - O(H)
     */
     public static Node traverseBT(Node root,Node p, Node q){
